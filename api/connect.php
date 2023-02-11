8f33e5656204f75b43f6b4f2f143043c04c1d9f7
islam hegazy <36385636+islamhegazy@users.noreply.github.com>
2023-02-11 16:47:09 +0200

    Update connect.php

--- api/connect.php
+++ api/connect.php
@@ -1,10 +1,10 @@
 <?php
 
 // db credentials
-define('DB_HOST', 'z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('DB_USER', 'kbf2dxt47dvfdbnv');
-define('DB_PASS', 'bz1rj16lzhlhf0s9');
-define('DB_NAME', 'v6bh103mtrr1js6o');
+define('DB_HOST', 'c8u4r7fp8i8qaniw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+define('DB_USER', 'f5bvkmwltrg7ks15');
+define('DB_PASS', 'c4vuspk6l2y4zjzz');
+define('DB_NAME', 'e8m6bibij2vmgn1z');
 
 // Connect with the database.
 function connect() {
